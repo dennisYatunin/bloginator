96f8d412a052bdef77f75f21a2fae601aa45add5
Nicholas Yang <nick@nicholasyang.com>
2015-10-18 12:49:39 -0400

    minor changes

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+## Checks the username and password with the utils function auth()
+    
 @app.route('/login', methods=['GET','POST'])
 def login():
 
@@ -23,25 +25,18 @@ def login():
             session['userid'] = userid
             return redirect(url_for('home'))
         else:
-            return """ <h1> Bad Username or Password </h1> """
+            return render_template("login.html", err="Incorrect password or username")
 
     else:
         return render_template("login.html")
 
 
-    # if request.method = 'POST':
-    #     user = request.form['username']
-    #     pswd = request.form['password']
-    #     if(util.auth(user,pswd)){
-    #         session['loggedin']= True
-
-    # return render_template("login.html")
-
-
+# Pops the session. Then sends the user to a logged out page
 @app.route("/logout")
 def logout():
     session.pop('username', None)
-    return redirect("logout.html")
+    return render_template("logout.html")
+
 
 @app.route('/new', methods='POST')
 def new():
@@ -49,19 +44,17 @@ def new():
     line = request.form['line']
     return render_template("newstory.html")
 
+# Very similar to log in, just checks whether the two passwords are the same, then adds the user using a utils function
 @app.route('/register')
 def register():
     if request.method == "POST":
         username = request.form['username']
         password = request.form['password']
-      
-        if button == "cancel":
-            return redirect(url_for('home'))
         if (request.form['password2'] != password):
             return """ <h1> Error, passwords are not the same </h1> """
         else:
             utils.addUser(username, password)
-            return redirect(url_for('home'))
+            return redirect(url_for('new'))
     else:
         return render_template("register.html")
 
--- utils.py
+++ utils.py
@@ -1,35 +1,37 @@
 
 import sqlite3
-conn = sqlite3.connect("blog.db")
-c = conn.cursor()
-#takes in username and pw (plaintext for now) and returns boolean
+
+#takes in username and pw (plaintext for now) and returns userid or -1 if invalid
 #if credentials are valid
 
-
-
 def getStory(story_id):
-	c.execute('SELECT data FROM lines WHERE story_id = ?', (str(story_id)))
-	lines = c.fetchall()[0]
-	print lines
-	story = ""
-	for i in lines:
-		print i
-		story += i[0] + " \n"
-	return  story
-    
+    conn = sqlite3.connect("blog.db")
+    c = conn.cursor()
+    c.execute('SELECT data FROM lines WHERE story_id = ?', (str(story_id)))
+    lines = c.fetchall()[0]
+    print lines
+    story = ""
+    for i in lines:
+        print i
+        story += i[0] + " \n"
 
+    return story    
 
 #if valid cred, return user_id
 #else return -1
-def auth(user, pw):    
+def auth(user, pw):
+    conn = sqlite3.connect("blog.db")
+    c = conn.cursor()
     q = "SELECT rowid FROM users where username = ? and pw = ? "
     c.execute(q, (user, pw) )
     result = c.fetchall()
     if len(result) == 1:
-    	return result[0][0]
+        return result[0][0]
     return -1
 
 def addUser(user, pw):
+    conn = sqlite3.connect("blog.db")
+    c = conn.cursor()
     c.execute("INSERT INTO users VALUES (?, ?)", (user, pw))
     
 
