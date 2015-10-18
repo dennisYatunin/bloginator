5c11ae54de631815926244ef419394b282fbb1f8
Nicholas Yang <nick@nicholasyang.com>
2015-10-18 12:54:50 -0400

    merged...again

--- utils.py
+++ utils.py
@@ -1,14 +1,13 @@
 
 import sqlite3
-<<<<<<< HEAD
-=======
+
 conn = sqlite3.connect("blog.db", check_same_thread=False)
 c = conn.cursor()
 
 #takes in username and pw (plaintext for now) and returns boolean
 #if credentials are valid
 
->>>>>>> d8891613cecf9f665eb634a629b163bc79ea7a9e
+
 
 #takes in username and pw (plaintext for now) and returns userid or -1 if invalid
 #if credentials are valid
@@ -32,24 +31,7 @@ def getStory(story_id):
 #if valid cred, return user_id
 #else return -1
 def auth(user, pw):
-<<<<<<< HEAD
-    conn = sqlite3.connect("blog.db")
-    c = conn.cursor()
-    q = "SELECT rowid FROM users where username = ? and pw = ? "
-    c.execute(q, (user, pw) )
-    result = c.fetchall()
-    if len(result) == 1:
-        return result[0][0]
-    return -1
 
-def addUser(user, pw):
-    conn = sqlite3.connect("blog.db")
-    c = conn.cursor()
-    c.execute("INSERT INTO users VALUES (?, ?)", (user, pw))
-    
-
-
-=======
     q = "SELECT rowid FROM users where username = ? and pw = ?;"
     c.execute(q, (user, pw) )
     result = c.fetchall()
@@ -61,4 +43,4 @@ def addUser(user, pw):
 def addUser(user, pw):
     c.execute("INSERT INTO users VALUES (?, ?);", (user, pw))
     conn.commit()
->>>>>>> d8891613cecf9f665eb634a629b163bc79ea7a9e
+
